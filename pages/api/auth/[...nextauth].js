8504e986b76d0e48a3ff433a97af6d18cc5edb60
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-04-21 20:19:53 +0530

    created strapi application and installed next-auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,7 @@
+import NextAuth from "next-auth";
+import { FirestoreAdapter } from "@next-auth/firebase-adapter";
+import { firestore } from "@/lib/firestore";
+
+export default NextAuth({
+  adapter: FirestoreAdapter(firestore),
+});
