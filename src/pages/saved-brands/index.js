8a5d4b0af6403c07a8636df20b92dd81b86269d8
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-05-30 14:43:52 +0530

    Implemented protected routes

--- src/pages/saved-brands/index.js
+++ src/pages/saved-brands/index.js
@@ -1,23 +1,32 @@
 import axios from "axios";
-import React from "react";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import React, { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useQuery } from "@tanstack/react-query";
 
 import Loader from "@/components/Loader";
 import BrandCard from "@/components/BrandCard";
+import { useRouter } from "next/router";
 
 const likedBrands = async () => {
   const response = await axios.get("/api/user/getLikedBrands");
   return response.data;
 };
+
 const Brands = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    status === "unauthenticated" && router.replace("/auth/signin");
+  }, [status]);
+
   const { data, error, isLoading } = useQuery({
     queryFn: likedBrands,
     queryKey: ["liked-brands"],
   });
+
   if (isLoading) return <Loader />;
   if (error) return "An error has occurred: " + error.message;
-
-  // console.log(data.data);
 
   return (
     <div className='grid grid-cols-1 gap-2 md:gap-6'>
