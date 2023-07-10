83586e793fcfaa6b45ea3195d751bfcfcb6f1a75
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-07-10 07:55:46 +0530

    callback next-auth changes

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,17 +1,3 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
 export default function CTA() {
   return (
     <div className='relative sm:py-16 mt-12'>
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -15,16 +15,13 @@ export const authOptions = {
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
-      profile(profile) {
-        return profile;
-      },
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      profile(profile) {
-        return profile;
-      },
+      // profile(profile) {
+      //   return profile;
+      // },
     }),
   ],
   // pages: {
