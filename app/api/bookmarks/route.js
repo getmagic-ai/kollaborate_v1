d38b6deabfdd00e3f490d13abeab5443d9818c89
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-07-15 14:44:16 +0530

    Added dynamice force

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -1,12 +1,12 @@
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-
+export const dynamic = "force-dynamic";
 export async function GET(req, res) {
   try {
     // Process a GET request
     const { userId } = auth();
-    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+    if (!userId) return NextResponse("Unauthorized", { status: 401 });
 
     const savedBrands = await prismadb.saved.findMany({
       where: {
@@ -25,6 +25,6 @@ export async function GET(req, res) {
     return NextResponse.json(bookmarkedBrands);
   } catch (error) {
     console.log(error);
-    NextResponse.json({ error: "Request method not allowed" });
+    return NextResponse.json({ error: "Request method not allowed" });
   }
 }
