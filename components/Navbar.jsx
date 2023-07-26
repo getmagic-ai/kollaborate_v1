155de0317c0c87d01ce51decbe3cac86a5f272fe
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-07-26 19:33:48 +0530

    fixed async await in client component error

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
 import MainNav from "@/components/MainNav";
