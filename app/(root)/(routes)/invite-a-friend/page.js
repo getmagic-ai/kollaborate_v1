186d17876a998a30bc9817c2aea1c8f342886acb
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-08-23 16:09:17 +0530

    Added sendgrid email invitation

--- app/(root)/(routes)/invite-a-friend/page.js
+++ app/(root)/(routes)/invite-a-friend/page.js
@@ -1,7 +1,33 @@
 import CTA from "@/components/CTA";
-import React from "react";
+import sgMail from "@sendgrid/mail";
+import toast from "react-hot-toast";
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const InviteAFriend = () => {
+  const [email, setEmail] = useState("");
+
+  const inviteFriend = () => {
+    const msg = {
+      to: email,
+      from: "test@example.com", // Use the email address or domain you verified above
+      subject: "Sending with Twilio SendGrid is Fun",
+      text: "and easy to do anywhere, even with Node.js",
+      html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+    };
+    sgMail.send(msg).then(
+      () => {
+        toast.success("Invite sent successfully");
+      },
+      (error) => {
+        console.error(error);
+
+        if (error.response) {
+          console.error(error.response.body);
+        }
+      }
+    );
+  };
+
   return (
     <div className='mx-auto max-w-7xl py-6 px-6 lg:px-8'>
       <h3 className='text-3xl font-bold tracking-tight text-white sm:text-4xl'>
