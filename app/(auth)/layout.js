38aff079065b79971fc134f7059ea5fecda00c75
Prathmesh Sadake <prathmeshsadake@gmail.com>
2024-02-21 14:11:44 +0530

    Add new files, delete unused files, and update configurations

--- app/(auth)/layout.js
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }) {
-  return (
-    <div className='bg-gray-900 flex items-center justify-center h-screen w-full'>
-      {children}
-    </div>
-  );
-}
