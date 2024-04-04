b6b33df12282704f34dce12061273062e52d60dc
andylnkd <dhalwani1@gmail.com>
2024-04-04 13:16:41 -0700

    moved Search API to an env var to switch as needed

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -5,12 +5,15 @@ export async function GET(req: Request) {
   const url = new URL(req.url);
   const searchQuery = url.searchParams.get("query") || "";
   const decodedQuery = decodeURIComponent(searchQuery);
+  
   try {
     // Process a GET request
     const { data } = await axios.get(
-      `http://api.kollaborate.co/open-search?message=${decodedQuery}&page=1`
+      // `http://api.kollaborate.co/open-search?message=${decodedQuery}&page=1`
+      process.env.SEARCH_API_URL+`?message=${decodedQuery}&page=1`
     );
     return NextResponse.json(data);
+    
   } catch (error) {
     console.log(error);
     return new NextResponse("Internal Error", { status: 500 });
